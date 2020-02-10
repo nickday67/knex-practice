abae2b545929903986cbdaeeb56ccdc31ae015df
Nick Day <thenickday@icloud.com>
2020-02-10 15:39:27 -0500

    more practice

--- src/practice.js
+++ src/practice.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const knex = require('knex')
 
 const knexInstance = knex({
@@ -5,4 +6,45 @@ const knexInstance = knex({
     connection: process.env.DB_URL
 })
 
-console.log('knex and driver installed correctly');
+const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({ name: 'Point of view gun' })
+  .first()
+  .toQuery()
+//   .then(result => {
+//     console.log(result)
+//   })
+
+console.log(qry);
+
+const searchTerm = 'holo'
+
+function searchByProduceName(searchTerm) {
+    knexInstance
+      .select('product_id', 'name', 'price', 'category')
+      .from('amazong_products')
+      .where('name', 'ILIKE', `%${searchTerm}%`)
+      .then(result => {
+        console.log(result)
+    })
+}
+  
+searchByProduceName('holo')
+
+// searchByProduceName('holo')
+
+function paginateProducts(page) {
+    const productsPerPage = 10
+    const offset = productsPerPage * (page - 1)
+    knexInstance
+      .select('product_id', 'name', 'price', 'category')
+      .from('amazong_products')
+      .limit(productsPerPage)
+      .offset(offset)
+      .then(result => {
+        console.log(result)
+    })
+}
+  
+paginateProducts(2)
